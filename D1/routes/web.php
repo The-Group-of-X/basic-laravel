14bd9920f89daf4386558a1a40945865ff8c8bd4
Adish Dahal <me.adishdahal@gmail.com>
2025-08-03 20:47:31 +0545

    here

--- D1/routes/web.php
+++ D1/routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RanjitController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SanjibController;
 
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::get('/sanjib', [SanjibController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 
-Route::get('/ranju', [RanjitController::class, 'ranju']);
+Route::prefix('student')->name('student.')->group(function () {
+    Route::get('/sanjib', [SanjibController::class, 'index'])->name('sanjib');
+    Route::get('/ranju', [RanjitController::class, 'ranju'])->name('ranju');
+});
+
+
+
+//student.ranju
 
 
 
@@ -21,8 +27,15 @@ Route::get('/teesdfsdfseeeeam', function () {
 })->name('team');
 
 
-// Route::get('/{username}', function ($username = null) {
-//     return view('info', [
-//         'u' => $username ?? 'No User Name Available'
-//     ]);
-// });
+
+Route::prefix('attendances')->name('attendances.')->controller(AttendanceController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/analytics', 'analytics')->name('analytics');
+    Route::post('/check-in', 'checkIn')->name('checkIn');
+    Route::post('/check-out', 'checkOut')->name('checkOut');
+    Route::post('/start-break', 'startBreak')->name('startBreak');
+    Route::post('/end-break', 'endBreak')->name('endBreak');
+    Route::get('/{attendance}/todos', 'getTodos')->name('getTodos');
+    Route::patch('/todos/{todo}/update-status', 'updateTodo')->name('todosupdateStatus');
+    Route::delete('/{attendance}', 'destroy')->name('destroy');
+});
